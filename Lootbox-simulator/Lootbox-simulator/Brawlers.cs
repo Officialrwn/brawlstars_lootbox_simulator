4a2b450af51c481d34f59752ed7feef8545754cc
leo <vanleotran@gmail.com>
2020-06-07 06:39:20 +0300

    +Random chance for epic brawler

---
+++ Lootbox-simulator/Lootbox-simulator/Brawlers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lootbox_simulator
+{
+    public class Brawlers
+    {
+        Random random = new Random();
+        private List<string> epicBrawlers = new List<string>() { "Piper", "Pam", "Frank", "Bibi", "Bea", "Nani" };
+        private List<string> myBrawlers = new List<string>();
+        private bool isEmpty;
+        private int count = 0;
+
+        public void OpenBox()
+        {
+            //Console.Clear();
+
+            count++;
+            var chance = random.Next(1, 101);
+
+            if(chance <= 5 && !isEmpty)
+            {
+                Epic();
+            }
+            else
+            {
+                Console.WriteLine("No reward from box " +count);
+            }
+
+        }
+        public void Epic()
+        {
+            if (epicBrawlers.Count != 0)
+            {
+                var bIndex = random.Next(epicBrawlers.Count);
+                var newBrawler = epicBrawlers[bIndex];
+
+                Console.WriteLine("Unlocked Epic Brawler from box {0}: {1}" ,count, newBrawler);
+                myBrawlers.Add(newBrawler);
+                epicBrawlers.Remove(newBrawler);
+                Console.WriteLine("Current Brawlers in list:");
+                myBrawlers.ForEach(Console.WriteLine);
+            }
+            else
+            {
+                Console.WriteLine("All brawlers unlocked! :)");
+                isEmpty = true;
+            }
+        }
+    }
+}
--- Lootbox-simulator/Lootbox-simulator/Program.cs
+++ Lootbox-simulator/Lootbox-simulator/Program.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace Lootbox_simulator
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("2nd commit testing changes");
+            var brawler = new Brawlers();
+
+            Console.WriteLine("Press enter to get random brawler from list or type exit to stop program");
+            var isTrue = true;
+            do
+            {
+                var input = Console.ReadLine().ToLower();
+                if (input != "exit")
+                {
+                    brawler.OpenBox();
+                }
+                else isTrue = false;
+            } while (isTrue);
         }
     }
 }
